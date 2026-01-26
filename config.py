ab09238947c895fb83c4dfab90c372c3a37e9dd1
Олійник Гліб <ipz242_ogi@student.ztu.edu.ua>
2026-01-26 15:45:44 +0200

    Виправлено помилку в config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+if os.getenv("ENV") != "production":
+    from dotenv import load_dotenv
+    load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
